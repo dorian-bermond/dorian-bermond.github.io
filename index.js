f82a827b0037251da6e6d921fa01a9fb86750f3c
Dorian <dorian.bermond@gmail.com>
2019-11-11 18:20:23 +0800

    SG v1.4.2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.getElementById('Head').addEventListener("onmousehover", Hover) ;
+console.log("Get") ;
 
 function Hover()
 {
+  console.log("Hovered") ;
   document.getElementByClass("arm").setAttribute("y2", 175) ;
 }
