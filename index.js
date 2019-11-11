c3394ec5bfb94e49d457fd98c7a66f820dd58b6e
Dorian <dorian.bermond@gmail.com>
2019-11-11 19:02:04 +0800

    SG v2.0

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //document.getElementById('Head').addEventListener("onmouseover", Hover) ;
-console.log("Get") ;
+//console.log("Get") ;
 
 function Hover()
 {
   console.log("Hovered") ;
-  document.getElementByClass("arm").setAttribute("y2", 175) ;
+  document.getElementById("leftArm").setAttribute("y2", 150) ;
+  document.getElementById("rightArm").setAttribute("y2", 150) ;
+  document.getElementById("mouth").setAttribute("d", "M 150 140 C 150 120, 200 120, 200 140") ;
+}
+
+function notHover()
+{
+  console.log("Not hovered") ;
+  document.getElementById("leftArm").setAttribute("y2", 200) ;
+  document.getElementById("rightArm").setAttribute("y2", 200) ;
+  document.getElementById("mouth").setAttribute("d", "M 150 120 C 150 140, 200 140, 200 120") ;
 }
